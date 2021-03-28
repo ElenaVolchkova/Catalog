7cefac985a4474cccc77d39fd5051dc77d7cc403
ElenaVolchkova <sombradzi@gmail.com>
2021-03-28 22:16:34 +0300

    Fixed template, added folder of templates

--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from employee.models import Employee
 
 # Create your views here.
 def employee_list(request):
-    return render(request, 'employee/employee_list.html', {})
+	employees = Employee.objects.all()
+	# context = {"employee": employee}
+	return render(request, 'employee/employee_list.html', {"employee_list": employees})
