fb2a58b3605defe4518fda53f27ac4d2a93ccd90
ElenaVolchkova <sombradzi@gmail.com>
2021-03-28 20:21:44 +0300

    first commit

---
+++ employee/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    salary = models.FloatField()
+    employment_date = models.DateTimeField(blank=True, null=True)
+    information_about_the_paid_salary = models.FloatField()
+
+    def save(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
