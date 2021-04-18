9599ca71cd5006c9d7d383b4263171214963bd9c
ElenaVolchkova <sombradzi@gmail.com>
2021-04-19 01:47:53 +0300

    Changed model Employee, added filter for employees

--- employee/admin.py
+++ employee/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Employee
 
-admin.site.register(Employee)
+
+# admin.site.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'chief', 'salary', 'paid_salary')
+    list_filter = ('position', 'level')
+
+
+admin.site.register(Employee, EmployeeAdmin)
--- employee/models.py
+++ employee/models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 
 class Employee(models.Model):
+    СЕО = 'СЕО'
+    MIDDLE = 'MD'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    QA = 'QA'
+    POSITION_CHOICES = [
+        (СЕО, 'СЕО'),
+        (MIDDLE, 'Middle'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+        (QA, 'QA'),
+    ]
+    position = models.CharField(
+        max_length=20,
+        choices=POSITION_CHOICES,
+        default=JUNIOR,
+    )
     name = models.CharField(max_length=200)
-    position = models.CharField(max_length=200)
     salary = models.FloatField()
-    employment_date = models.DateTimeField(blank=True, null=True)
+    employment_date = models.DateField(null=True, blank=True)
     paid_salary = models.FloatField()
-    chief = models.ForeignKey("self", on_delete=models.SET_NULL, null=True)
+    chief = models.ForeignKey("employee.Employee", on_delete=models.SET_NULL, null=True, blank=True)
     level = models.IntegerField(default=0)
 
     def __str__(self):
